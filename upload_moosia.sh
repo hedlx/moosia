ce08adcb74dbd7d64d892747c73d0ade3f4917b2
Albert Safin <xzfcpw@gmail.com>
2020-02-29 08:12:24 +0000

    v1

--- upload_moosia.sh
+++ upload_moosia.sh
@@ -10,8 +10,30 @@ tg_chat_id="�"
 tg_curl() { curl "https://api.telegram.org/bot$tg_token/$@"; echo; }
 trap 'tg_curl "sendMessage?chat_id=$tg_chat_id&text=Something%20wrong"' ERR
 
-tmp_vid=$(mktemp "/tmp/tg_upload_XXXX.mp4")
-trap 'rm -vf "$tmp_vid"' EXIT
+tmp=$(mktemp --directory --tmpdir moosia_uploader_XXXXX)
+trap 'rm -rvf "$tmp"' EXIT
 
-ffmpeg -y -loglevel error -i "$1" "$tmp_vid"
-tg_curl "sendVideo?chat_id=$tg_chat_id" -F "video=@$tmp_vid"
+# Extract a sane filename
+vid_name=${1##*/}
+vid_name=${vid_name%%.*}
+vid_name=${vid_name//[!0-9a-zA-Z_-]/}
+
+# Convert to get better compression
+ffmpeg -y -loglevel error -i "$1" -c:v libx265 "$tmp/full.mp4"
+
+# Generate thumbnail
+ffmpeg -y -loglevel error -i "$tmp/full.mp4" \
+    -vf "thumbnail,scale=w=320:h=320:force_original_aspect_ratio=decrease" \
+    -frames:v 1 "$tmp/thumb.jpg"
+
+# Read some metadata
+IFS=, read width height duration < <(
+    ffprobe -v error -select_streams v:0 \
+        -show_entries stream=width,height,duration -of csv=p=0 \
+        "$tmp/full.mp4")
+
+# Upload
+tg_curl \
+    "sendVideo?chat_id=$tg_chat_id&width=$width&height=$height&duration=$duration&supports_streaming=True" \
+    -F "video=@$tmp/full.mp4;filename=Moosia_$vid_name.mp4" \
+    -F "thumb=@$tmp/thumb.jpg"
