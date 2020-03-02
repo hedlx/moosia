46ee9698414e91b7e905ce3a3cf5cc6668da2388
Albert Safin <xzfcpw@gmail.com>
2020-03-02 09:04:51 +0000

    py

--- upload_moosia.py
+++ upload_moosia.py
@@ -1,26 +1,89 @@
 #!/usr/bin/env python3
 
-import sys, tempfile, telegram
+import json, os, re, subprocess, sys, tempfile
+import telegram
 
-# ./upload.py <chat_id> %f %n
+# Usage: ./upload_moosia.py <tg_token> <tg_chat_id> %f %n
 
-chat_id = int(sys.argv[1])
-file_name = sys.argv[2]
-file_type = sys.argv[3]
+tg_token = sys.argv[1]
+tg_chat_id = int(sys.argv[2])
+file_name = sys.argv[3]
 
-if sys.argv[3] == "32": # FTYPE_MPEG_TIMELAPSE
+if sys.argv[4] == "32":  # FTYPE_MPEG_TIMELAPSE
     timelapse = True
-elif sys.argv[3] == "16": # FTYPE_MPEG_MOTION
+elif sys.argv[4] == "16":  # FTYPE_MPEG_MOTION
     timelapse = False
 else:
     exit(1)
 
 tmp = tempfile.TemporaryDirectory(prefix="moosia-uploader-")
 
-tg = telegram.Bot("�")
+# Extract a sane filename
+vid_name = os.path.splitext(os.path.basename(file_name))[0]
+vid_name = re.sub("[^0-9a-zA-Z_-]", "", vid_name)
 
-#["ffmpeg", "-y", "-loglevel", "error", "-i", file_name, "-c:v", "libx265", "/full.mp4"]
+# Convert to get better compression
+subprocess.run(
+    args=[
+        "ffmpeg",
+        "-y",
+        "-loglevel",
+        "error",
+        "-i",
+        file_name,
+        "-c:v",
+        "libx265",
+        f"{tmp.name}/{vid_name}.mp4",
+    ],
+    check=True,
+)
 
-print(tmp)
+# Generate thumbnail
+subprocess.run(
+    check=True,
+    args=[
+        "ffmpeg",
+        "-y",
+        "-loglevel",
+        "error",
+        "-i",
+        f"{tmp.name}/{vid_name}.mp4",
+        "-vf",
+        "thumbnail,scale=w=320:h=320:force_original_aspect_ratio=decrease",
+        "-frames:v",
+        "1",
+        f"{tmp.name}/thumb.jpg",
+    ],
+)
 
-#tg.send_message(chat_id, "test")
+# Read some metadata
+metadata = json.loads(
+    subprocess.run(
+        check=True,
+        stdout=subprocess.PIPE,
+        args=[
+            "ffprobe",
+            "-v",
+            "error",
+            "-print_format",
+            "json",
+            "-show_streams",
+            "-select_streams",
+            "v:0",
+            f"{tmp.name}/{vid_name}.mp4",
+        ],
+    ).stdout
+)["streams"][0]
+
+# Upload
+tg = telegram.Bot(tg_token)
+tg.send_video(
+    chat_id=tg_chat_id,
+    caption="#timelapse" if timelapse else None,
+    video=open(f"{tmp.name}/{vid_name}.mp4", "rb"),
+    thumb=open(f"{tmp.name}/thumb.jpg", "rb"),
+    duration=round(float(metadata["duration"])),
+    width=int(metadata["width"]),
+    height=int(metadata["height"]),
+    supports_streaming=True,
+)
